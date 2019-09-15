482d2f6a1f791c076315f0ed003e7d56b2413560
gianlucaparadise <gianlucaparadise@gmail.com>
2019-09-15 18:15:34 +0200

    Chromecast integration

--- js/receiver.js
+++ js/receiver.js
@@ -2,4 +2,12 @@ import { log } from "./logger.js";
 import { app } from "./app.js";
 import { recipe } from "./stub-instructions.js";
 
-setTimeout(() => app.start(recipe), 2000);
+const context = cast.framework.CastReceiverContext.getInstance();
+context.addCustomMessageListener('urn:x-cast:cast-your-instructions', event => {
+    log(() => 'CustomMessage: ');
+    log(() => event);
+    const recipe = event.data;
+    app.start(recipe);
+});
+
+context.start();

eb6365ec62f8d885b18410fcb4000e3e0ebf7c18
gianlucaparadise <gianlucaparadise@gmail.com>
2019-10-19 06:48:53 +0200

    Vue App Refactor: Sample Vue app init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <SplashScreen :isReady="isReady" />
+    <HelloWorld v-if="!isLoading" msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+
+export default {
+  name: "app",
+  data: () => ({
+    isReady: false
+  }),
+  components: {
+    HelloWorld,
+    SplashScreen
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isReady = true;
+    }, 1500);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+</style>
